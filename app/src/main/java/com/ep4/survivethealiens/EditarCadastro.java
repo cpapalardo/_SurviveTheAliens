01014e49e96b0311fb3d816855f83ab4113d2ed8
Nathalia Buscaratti Silva <Nathalia Buscaratti Silva>
2016-11-08 15:36:05 -0200

    Tela de Ediçao de Cadastro

---
+++ app/src/main/java/com/ep4/survivethealiens/EditarCadastro.java
@@ -0,0 +1,13 @@
+package com.ep4.survivethealiens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditarCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_cadastro);
+    }
+}
